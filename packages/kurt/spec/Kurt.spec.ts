674f8433c3893d687d1f767352ddb62093b1e10f
Joe McIlvain <joe.eli.mac@gmail.com>
2024-05-10 15:15:03 -0700

    feat: add `KurtSamplingOptions`, for tweaking output sampling details
    This commit adds a `KurtSamplingOptions` type with various tweakable values and a
    `KurtSamplingOptionsDefault` constant with the default values to use when not given.
    
    The `sampling` key can be used to specify sampling options on creation, and/or on individual
    generate calls. See the docs and tests for more details on what settings are possible and how they
    behave.
    
    A future PR will update the adapter libraries to start making use of these values.

---
+++ packages/kurt/spec/Kurt.spec.ts
@@ -0,0 +1,122 @@
+import { describe, expect, test } from "@jest/globals"
+import { FakeAdapterV1 } from "./FakeAdapterV1"
+import {
+  Kurt,
+  type KurtCreateOptions,
+  type KurtSamplingOptions,
+  KurtSamplingOptionsDefault,
+} from "../src/Kurt"
+
+function createV1(options: KurtCreateOptions = {}) {
+  const adapter = new FakeAdapterV1()
+  const kurt = new Kurt(adapter, options)
+  return { adapter, kurt }
+}
+
+describe("Kurt", () => {
+  describe("sampling options", () => {
+    test("using entirely default values by default", async () => {
+      const { adapter, kurt } = createV1()
+
+      kurt.generateNaturalLanguage({ prompt: "Hello!" })
+
+      expect(adapter.lastGenerateRawEventsCall?.sampling).toEqual(
+        KurtSamplingOptionsDefault
+      )
+    })
+
+    test("using full overrides on creation", async () => {
+      const sampling: Required<KurtSamplingOptions> = {
+        maxOutputTokens: 1024,
+        temperature: 0.1,
+        topP: 0.5,
+      }
+      const { adapter, kurt } = createV1({ sampling })
+
+      kurt.generateNaturalLanguage({ prompt: "Hello!" })
+
+      expect(adapter.lastGenerateRawEventsCall?.sampling).toEqual(sampling)
+    })
+
+    test("using full overrides on generate call", async () => {
+      const { adapter, kurt } = createV1()
+
+      const sampling: Required<KurtSamplingOptions> = {
+        maxOutputTokens: 1024,
+        temperature: 0.1,
+        topP: 0.5,
+      }
+      kurt.generateNaturalLanguage({ prompt: "Hello!", sampling })
+
+      expect(adapter.lastGenerateRawEventsCall?.sampling).toEqual(sampling)
+    })
+
+    test("using partial overrides at each layer", async () => {
+      const { adapter, kurt } = createV1({
+        sampling: {
+          maxOutputTokens: 999, // will be shadowed by the generate call value
+          temperature: 0.1, // will not be shadowed
+        },
+      })
+
+      kurt.generateNaturalLanguage({
+        prompt: "Hello!",
+        sampling: { maxOutputTokens: 1024 },
+      })
+
+      expect(adapter.lastGenerateRawEventsCall?.sampling).toEqual({
+        ...KurtSamplingOptionsDefault,
+        temperature: 0.1,
+        maxOutputTokens: 1024,
+      })
+    })
+
+    test("complains when values violate certain bounds", () => {
+      const { kurt } = createV1()
+
+      expect(() =>
+        kurt.generateNaturalLanguage({
+          prompt: "Hello!",
+          sampling: { maxOutputTokens: 0 },
+        })
+      ).toThrow("maxOutputTokens must be at least 1 (got: 0)")
+
+      expect(() =>
+        kurt.generateNaturalLanguage({
+          prompt: "Hello!",
+          sampling: { temperature: -0.1 },
+        })
+      ).toThrow("temperature must be no less than 0 (got: -0.1)")
+
+      expect(() =>
+        kurt.generateNaturalLanguage({
+          prompt: "Hello!",
+          sampling: { topP: -0.1 },
+        })
+      ).toThrow("topP must be no less than 0 (got: -0.1)")
+
+      expect(() =>
+        kurt.generateNaturalLanguage({
+          prompt: "Hello!",
+          sampling: { topP: 1.1 },
+        })
+      ).toThrow("topP must be no greater than 1 (got: 1.1)")
+    })
+
+    test("silently coerces values when violating certain other constraints", () => {
+      const { adapter, kurt } = createV1()
+
+      kurt.generateNaturalLanguage({
+        prompt: "Hello!",
+        sampling: { maxOutputTokens: 1023.5, temperature: 0, topP: 0 },
+      })
+
+      expect(adapter.lastGenerateRawEventsCall?.sampling).toEqual({
+        ...KurtSamplingOptionsDefault,
+        maxOutputTokens: 1024,
+        temperature: Number.MIN_VALUE,
+        topP: Number.MIN_VALUE,
+      })
+    })
+  })
+})
