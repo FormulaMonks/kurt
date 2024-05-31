3cb74f9979097078d72ad8eb59cd2cf5503b605a
Joe McIlvain <joe.eli.mac@gmail.com>
2024-05-31 12:07:42 -0700

    feat: support parallel tool calls in `KurtVertexAI` with `generateWithOptionalTools`
    This commit updates the `KurtVertexAI` adapter to handle the case of multiple
    parallel tool calls from the LLM, when being used with the `generateWithOptionalTools`
    method of `Kurt`.
    
    Similar to PR #35, this PR resolves part of issue #33

--- packages/kurt-vertex-ai/src/KurtVertexAI.ts
+++ packages/kurt-vertex-ai/src/KurtVertexAI.ts
@@ -265,31 +265,60 @@ async function* transformStreamWithOptionalTools<
     for (const [partIndex, part] of parts.entries()) {
       const chunk = part.text
       const isFinal =
-        (isContentFinal && partIndex === parts.length - 1) || part.functionCall
+        (isContentFinal || part.functionCall) && partIndex === parts.length - 1
 
       if (chunk) {
         chunks.push(chunk)
         yield { chunk }
       }
       if (isFinal) {
-        const { functionCall } = part
-        if (functionCall) {
-          const { name } = functionCall
-          const schema = tools[name]
-          if (!schema) {
-            throw new Error(
-              `Vertex AI tried to call tool ${name} which isn't in the tool set ${JSON.stringify(
-                Object.keys(tools)
-              )}}`
-            )
+        if (part.functionCall) {
+          const allData = parts.map((part) => {
+            if (!part.functionCall) {
+              throw new Error(
+                `Vertex AI mixed function calls with non-function calls in the same raw stream event: ${JSON.stringify(
+                  rawEvent
+                )}`
+              )
+            }
+
+            const { name } = part.functionCall
+
+            const schema = tools[name]
+            if (!schema) {
+              throw new Error(
+                `Vertex AI tried to call tool ${name} which isn't in the tool set ${JSON.stringify(
+                  Object.keys(tools)
+                )}}`
+              )
+            }
+            return {
+              name,
+              args: applySchemaToFuzzyStructure(schema, part.functionCall),
+            } as D
+          })
+
+          // Emit a text chunk for each tool call (with line breaks in between).
+          for (const [dataIndex, data] of allData.entries()) {
+            if (dataIndex > 0) {
+              chunks.push("\n")
+              yield { chunk: "\n" }
+            }
+            const text = JSON.stringify(data.args)
+            chunks.push(text)
+            yield { chunk: text }
           }
-          const data = {
-            name,
-            args: applySchemaToFuzzyStructure(schema, functionCall),
-          } as D
-          const text = JSON.stringify(data.args)
-          yield { chunk: text }
-          yield { finished: true, text, data }
+
+          // biome-ignore lint/style/noNonNullAssertion: we already validated above that length > 0
+          const data = allData[0]!
+          const additionalData = allData.slice(1)
+          const text = chunks.join("")
+
+          if (additionalData.length > 0) {
+            yield { finished: true, text, data, additionalData }
+          } else {
+            yield { finished: true, text, data }
+          }
         } else {
           const text = chunks.join("")
           yield { finished: true, text, data: undefined }
