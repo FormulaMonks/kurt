63736b1460cf77ac740582088018a04f7fdd41c4
Joe McIlvain <joe.eli.mac@gmail.com>
2024-06-28 15:48:32 -0700

    feat: use new KurtError types from KurtOpenAI adapter
    This will allow application developers to have more options about how they handle errors.

--- packages/kurt-open-ai/spec/generateStructuredData.spec.ts
+++ packages/kurt-open-ai/spec/generateStructuredData.spec.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from "@jest/globals"
 import { z } from "zod"
-import { snapshotAndMock } from "./snapshots"
+import { snapshotAndMock, snapshotAndMockWithError } from "./snapshots"
+import { KurtResultValidateError } from "@formula-monks/kurt"
 
 describe("KurtOpenAI generateStructuredData", () => {
   test("says hello", async () => {
@@ -15,5 +16,38 @@ describe("KurtOpenAI generateStructuredData", () => {
       })
     )
     expect(result.data).toEqual({ say: "hello" })
+  })
+
+  test("throws a validate error from an impossible schema", async () => {
+    await snapshotAndMockWithError(
+      (kurt) =>
+        kurt.generateStructuredData({
+          prompt: "Say hello!",
+          schema: z
+            .object({
+              say: z
+                .string()
+                .regex(/(?=IMPOSSIBLE)hello/) // (can't be both "hello" and "IMPOSSIBLE")
+                .describe("A single word to say"),
+            })
+            .describe("Say a word"),
+        }),
+
+      (errorAny) => {
+        expect(errorAny).toBeInstanceOf(KurtResultValidateError)
+        const error = errorAny as KurtResultValidateError
+
+        expect(error.text).toEqual('{"say":"hello"}')
+        expect(error.data).toEqual({ say: "hello" })
+        expect(error.cause.issues).toEqual([
+          {
+            code: "invalid_string",
+            path: ["say"],
+            validation: "regex",
+            message: "Invalid",
+          },
+        ])
+      }
+    )
   })
 })
