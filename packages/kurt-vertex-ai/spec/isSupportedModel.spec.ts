603bb54beae391020737c5520be51e5b7bbd3528
Mehdi Chaabouni <mehdi.chaabouni@theoremone.co>
2025-02-19 09:10:21 -0500

    chore: update gemini supported models

--- packages/kurt-vertex-ai/spec/isSupportedModel.spec.ts
+++ packages/kurt-vertex-ai/spec/isSupportedModel.spec.ts
@@ -1,20 +1,20 @@
 import { expect, test } from "@jest/globals"
-import {
-  KurtVertexAI,
-  type KurtVertexAISupportedModel,
-} from "../src/KurtVertexAI"
+import { KurtVertexAI, type KurtVertexAISupportedModel } from "../src"
 
 test("KurtVertexAI.isSupportedModel", () => {
   // For updating this test, the current list of models can be found at:
-  // https://platform.openai.com/docs/models/overview
+  // https://ai.google.dev/gemini-api/docs/models/gemini
   expect(KurtVertexAI.isSupportedModel("gemini-1.5-pro")).toBe(true)
-  expect(KurtVertexAI.isSupportedModel("gemini-1.0-pro")).toBe(true)
+  expect(KurtVertexAI.isSupportedModel("gemini-1.5-flash")).toBe(true)
+  expect(KurtVertexAI.isSupportedModel("gemini-1.5-flash-8b")).toBe(true)
+  expect(KurtVertexAI.isSupportedModel("gemini-2.0-flash")).toBe(true)
+  expect(KurtVertexAI.isSupportedModel("gemini-1.0-pro")).toBe(false)
   expect(KurtVertexAI.isSupportedModel("gemini-1.0-pro-vision")).toBe(false)
 
   expect(KurtVertexAI.isSupportedModel("bogus")).toBe(false)
 
   // The below code proves that the function works as a type guard.
-  const modelName = "gemini-1.0-pro"
+  const modelName = "gemini-1.5-pro"
   if (KurtVertexAI.isSupportedModel(modelName)) {
     const modelNameGood: KurtVertexAISupportedModel = modelName
   }
