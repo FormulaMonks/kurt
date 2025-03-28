7ad923636b1b096663e1bdd2677bc01a9097ce0e
Mihail Brinza <mbrinza@firstam.com>
2025-03-28 19:41:11 +0000

    feat: add generic Web Search Kurt Tool

--- packages/kurt/spec/KurtSchema.spec.ts
+++ packages/kurt/spec/KurtSchema.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals"
-import { KurtSchema } from "../src/KurtSchema"
+import { KurtSchema } from "../src"
 import { zodToJsonSchema } from "zod-to-json-schema"
 import type { JSONSchema7 } from "json-schema"
 
